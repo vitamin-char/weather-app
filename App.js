41d3fc2f4166fef58b23e73a27897fedf8adff07
hayoung9000 <vitamin.char@gmail.com>
2019-09-23 18:16:30 +0900

    OpenWeatherMap API를 이용해서 날씨 정보 가져오기

--- App.js
+++ App.js
@@ -2,30 +2,51 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import Weather from './Weather';
 
+const API_KEY = "5e4e625cc839f97686866a56b9861d4e";
+
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-        isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude)
       },
       error =>{
-        console.log(error);
+        this.setState({
+          error: error
+        });
       }
     );
-  }
+  };
+
+  _getWeather = (lat, lon) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+      ) // 여기는 작은따옴표가 아니라 물결아이콘에 있는 그거!
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true
+      })
+    });
+  };
+
   render(){
-    const {isLoaded} = this.state;
+    const {isLoaded, error, temperature, name} = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {isLoaded ? <Weather/> : ( 
+        {isLoaded ? <Weather weatherName= {name} temp={Math.floor(temperature - 273.15)}/> : ( 
           <View style={styles.loading}> 
             <Text style={styles.loadingText}> Getting the weather</Text>
+            {error ? <Text style={styles.erorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -33,12 +54,15 @@ export default class App extends Component {
   }
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  erorText:{
+    color: "red",
+    backgroundColor:"transparent",
+    marginBottom: 48
   },
   loading:{
     flex: 1,
--- Weather.js
+++ Weather.js
@@ -2,22 +2,74 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {LinearGradient} from "expo-linear-gradient";
 import { Ionicons } from '@expo/vector-icons';
+import PropTypes from "prop-types";
 
-export default function Weather() {
+const weatherCases = {
+    Rain: {
+        colors: ['#00C6FB', '#005BEA'],
+        icon: 'ios-rainy',
+        title: 'Rain',
+        subtitle: '오늘은 비가 내려요. 우산을 꼭 챙기세요!'
+    },
+    Clear: {
+        colors: ['#ffffff', '#6de1ff'],
+        icon: 'ios-sunny',
+        title: 'Clear',
+        subtitle: '오늘은 맑아요'
+    },
+    Thunderstorm: {
+        colors: ['#415b72', '#004787'],
+        icon: 'ios-thunderstrom',
+        title: 'Thunderstorm',
+        subtitle: '오늘은 천둥 번개가 쳐요'    
+    },
+    Clouds: {
+        colors: ['#d7dde5', '#646970'],
+        icon: 'ios-cloudy',
+        title: 'Clouds',
+        subtitle: '오늘은 흐려요'
+    },
+    Snow: {
+        colors: ['#7DE2FC', '#B9B6E5'],
+        icon: 'ios-snow',
+        title: 'Snow',
+        subtitle: 'Do you wanna build a snowman?'
+    },  
+    Drizzle: {
+        colors: ['#d7dde5', '#c9d2d8', '#5db7fc'],
+        icon: 'ios-rainy-outline',
+        title: 'Drizzle',
+        subtitle: '오늘은 이슬비가 내려요'
+    },
+    Haze: {
+        colors: ['#d7dde5', '#646970'],
+        icon: 'ios-cloudy',
+        title: 'Haze',
+        subtitle: '오늘은 흐려요'
+    }
+}
+
+export default function Weather({temp, weatherName}) {
+    console.log(weatherName);
     return(
         <LinearGradient
-          colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.container}>
+          colors={weatherCases[weatherName].colors} style={styles.container}>
          <View style={styles.upper}>
-            <Ionicons color="white" size={144} name="ios-rainy"/>
-            <Text style={styles.temp}>35</Text>
+            <Ionicons color="white" size={144} name={weatherCases[weatherName].icon}/>
+            <Text style={styles.temp}>{temp}°</Text>
          </View>
          <View style={styles.lower}>
-            <Text style={styles.title}>Raining</Text>
-            <Text style={styles.subtitle}>For more info look outside</Text>
+            <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+            <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>
          </View>
         </LinearGradient>
     );
 }
+
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    weatherName: PropTypes.string.isRequired
+};
 
 const styles = StyleSheet.create({
     container:{
